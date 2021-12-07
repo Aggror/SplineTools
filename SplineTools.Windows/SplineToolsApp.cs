dc0fb3484281063eb1712266195bec56c29fc2fc
Aggror Desktop <aggror.jorn@gmail.com>
2021-12-07 12:42:00 +0100

    Base spline test project

---
+++ SplineTools.Windows/SplineToolsApp.cs
@@ -0,0 +1,15 @@
+using Stride.Engine;
+
+namespace SplineTools
+{
+    class SplineToolsApp
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
