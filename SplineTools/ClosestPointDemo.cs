aea68679fa36351409d866908c30474d222f6f29
Aggror Desktop <aggror.jorn@gmail.com>
2022-01-02 18:45:34 +0100

    Closest point on spline script

---
+++ SplineTools/ClosestPointDemo.cs
@@ -0,0 +1,62 @@
+﻿using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Engine.Splines.Components;
+using Stride.Input;
+
+namespace SplineTools
+{
+
+    public class ClosestPointDemo : SyncScript
+    {
+        public float MoveSpeed = 4.0f;
+        public SplineComponent spline;
+        public Entity closesPointOrb;
+
+        public override void Start()
+        {
+        }
+
+        public override void Update()
+        {
+            float deltaTime = (float)Game.UpdateTime.Elapsed.TotalSeconds;
+            Vector3 dir = new();
+
+            if (Input.HasKeyboard)
+            {
+                // Move with keyboard
+                // Forward/Backward
+                if (Input.IsKeyDown(Keys.W) || Input.IsKeyDown(Keys.Up))
+                {
+                    dir.Z += 1;
+                }
+                if (Input.IsKeyDown(Keys.S) || Input.IsKeyDown(Keys.Down))
+                {
+                    dir.Z -= 1;
+                }
+
+                // Left/Right
+                if (Input.IsKeyDown(Keys.A) || Input.IsKeyDown(Keys.Left))
+                {
+                    dir.X += 1;
+                }
+                if (Input.IsKeyDown(Keys.D) || Input.IsKeyDown(Keys.Right))
+                {
+                    dir.X -= 1;
+                }
+
+                if (dir.Length() != 0)
+                {
+                    //Update movement
+                    dir = Vector3.Normalize(dir);
+                    Entity.Transform.Position += dir * MoveSpeed * deltaTime;
+
+                    //Show closest point on spline
+                    var closestPositionInfo = spline.GetClosestPointOnSpline(Entity.Transform.WorldMatrix.TranslationVector);
+                    closesPointOrb.Transform.UseTRS = false;
+                    closesPointOrb.Transform.WorldMatrix.TranslationVector = closestPositionInfo.ClosestPosition;
+                    closesPointOrb.Transform.UpdateLocalFromWorld();
+                }
+            }
+        }
+    }
+}
