05aff9df584a29c223ef13259607a97d8e662900
Aggror Desktop <aggror.jorn@gmail.com>
2022-01-12 16:54:53 +0100

    Testing out spline by code

---
+++ SplineTools/SplineByCode.cs
@@ -0,0 +1,31 @@
+﻿using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Engine.Splines;
+using Stride.Engine.Splines.Components;
+using Stride.Input;
+
+namespace SplineTools
+{
+
+    public class SplineByCode : SyncScript
+    {
+        public override void Start()
+        {
+            var spline = new SplineComponent();
+            Entity.Add(spline);
+
+            //spline.SplineNodesC
+
+            //for (int i = 0; i < 3; i++)
+            //{
+                                                    
+            //}
+            
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+}
